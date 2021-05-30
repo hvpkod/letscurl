1b5cdcfabdfbc12474b0e96f2dc64c651f2c8b5f
Hampus <hampus.andersson@gmail.com>
2021-05-30 23:09:21 +0200

    Adding py files

---
+++ vecka.py
@@ -0,0 +1,35 @@
+import os
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://vecka.nu/"
+
+br = requests.get(url, headers={"User-agent": "Chrome"})
+sr = br.content
+bf = BeautifulSoup(sr, "html.parser")
+v = int(bf.find("time").text)
+
+html = """\
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Vecka</title>
+  <meta name="Veckor" content="Svenska veckor">
+
+
+</head>
+
+<body>
+{code}
+</body>
+</html>""".format(
+    code=v
+)
+
+f = open("vecka/index.html", "w+")
+f.write(html)
+f.close()
