da4d9598e03d4b165f35a5d0cdb1a108cba17f67
Hampus <hampus.andersson@gmail.com>
2021-05-31 08:17:50 +0200

    Pushing to to none index.html

--- namnsdag.py
+++ namnsdag.py
@@ -10,4 +10,5 @@ sr = br.content
 bf = BeautifulSoup(sr, "html.parser")
 v = bf.find("h1").text
 
-os.system("echo %s > namnsdag/index.html" % v)
+# os.system("echo %s > namnsdag/index.html" % v)
+os.system("echo %s > namnsdag" % v)
